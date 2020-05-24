e9e34bc15fa77e57fba8ed6e3f435f8a0ab7ad55
Hany <hany.eldessouky@gmail.com>
2020-05-24 19:46:02 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template:'<h1>test</h1>'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'projectzero';
+  name ='Hany ';
+  job='Software engineer'
 }
